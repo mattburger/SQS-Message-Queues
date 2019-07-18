01d96288a69c6cf89f0557e4c5bc81b3b48756f8
Matt Burger <matt.burger1@outlook.com>
2019-07-18 10:43:46 -0700

    QueuePublish and QueueClient basic

--- src/main/java/SQS/Message/Queues/QueueClient.java
+++ src/main/java/SQS/Message/Queues/QueueClient.java
@@ -1,4 +1,26 @@
 package SQS.Message.Queues;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+
+import java.util.List;
+
 public class QueueClient {
+
+    private AmazonSQS sqs;
+    private String queueName;
+    private String queueURL;
+    private List<Message> messages;
+
+    public QueueClient(String queueName) {
+        this.sqs = AmazonSQSClientBuilder.defaultClient();
+        this.queueName = queueName;
+        this.queueURL = this.sqs.getQueueUrl(this.queueName).getQueueUrl();
+    }
+
+    public void retrieveFromQueue() {
+        messages = sqs.receiveMessage(this.queueURL).getMessages();
+    }
 }
