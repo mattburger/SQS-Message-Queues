5b590945ddfe48a059293ab922454706b8385951
Matt Burger <matt.burger1@outlook.com>
2019-07-18 10:42:31 -0700

    class QueueClient logic added

--- src/main/java/SQS/Message/Queues/QueuePublisher.java
+++ src/main/java/SQS/Message/Queues/QueuePublisher.java
@@ -1,5 +1,30 @@
 package SQS.Message.Queues;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+
 public class QueuePublisher {
+
+    private AmazonSQS sqs;
+    private String queueName;
+    private String queueURL;
+
+    public QueuePublisher(String queueName) {
+        this.sqs = AmazonSQSClientBuilder.defaultClient();
+        this.queueName = queueName;
+        this.queueURL = this.sqs.getQueueUrl(this.queueName).getQueueUrl();
+    }
+
+    public void publishToQueue() {
+        double randNum = Math.random() * 1000;
+        int intRandNum = (int) randNum;
+
+        SendMessageRequest send_msg_request = new SendMessageRequest()
+                .withQueueUrl(this.queueURL)
+                .withMessageBody("Message with a random #: " + intRandNum)
+                .withDelaySeconds(5);
+        sqs.sendMessage(send_msg_request);
+    }
 
 }
